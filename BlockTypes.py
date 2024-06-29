4df0990c184be3794ebf1402219a26edef336a1e
niklas-walcher <niklaswalcher427@gmail.com>
2024-06-29 14:35:23 +0200

    added blocktypes

---
+++ BlockTypes.py
@@ -0,0 +1,15 @@
+#@author: niklas-walcher 
+#@date: 29-6-2024
+
+"""
+These are the blocktypes used in snake game.
+"""
+from __future__ import annotations
+from types import Any, Callable
+
+class BlockTypes:
+
+	FOOD: int = 0
+	SNAKE: int = 1
+	EMPTY: int = 2
+
